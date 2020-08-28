3bfc279f1c7dbefcca65acbd04302dd4c92374f5
ElvisLee <elvislee0725@gmail.com>
2020-08-27 23:47:44 -0700

    Modify eventlist table and pagination show after search

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-$( initializeApp );
+$(initializeApp);
 
 function initializeApp() {
-    const eventList = new EventList({
-        eventTitle: '#eventTitle',
-        eventType: '#eventType',
-        eventVenue: '#eventVenue',
-        eventCity: '#eventCity',
-        eventRange: '#eventRange',
-        sortBy: '#sort',
-        searchBtn: '#searchBtn',
-        eventMap: '#map',
-        noResultMsg: '#no-result',
-        prevBtn: '#prevBtn',
-        nextBtn: '#nextBtn',
-        pageNumber: '#pagination-number',
-        eventList: '#eventsList'
-    });
+  const eventList = new EventList({
+    eventTitle: '#eventTitle',
+    eventType: '#eventType',
+    eventVenue: '#eventVenue',
+    eventCity: '#eventCity',
+    eventRange: '#eventRange',
+    sortBy: '#sort',
+    searchBtn: '#searchBtn',
+    eventMap: '#map',
+    noResultMsg: '#no-result',
+    eventsPagination: '#eventsPagination',
+    prevBtn: '#prevBtn',
+    nextBtn: '#nextBtn',
+    pageNumber: '#pagination-number',
+    eventList: '#eventsList',
+  });
 }
